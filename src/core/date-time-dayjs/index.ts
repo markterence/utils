f454f5f565f87ce6bda541ea70491878b454dd41
Mark Terence Tiglao <github@markterence.me>
2025-02-25 14:08:57 +0800

    feat: implement date-time utilities using dayjs and add validation function

--- src/core/date-time-dayjs/index.ts
+++ src/core/date-time-dayjs/index.ts
@@ -1,3 +1,28 @@
-export default (): never => {
-  throw new Error('date-time-dayjs utility has not been implemented yet');
-};
+import dayjs from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import dayjsCalendar from 'dayjs/plugin/calendar';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import duration from 'dayjs/plugin/duration';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(dayjsCalendar);
+dayjs.extend(relativeTime);
+dayjs.extend(advancedFormat);
+dayjs.extend(timezone);
+dayjs.extend(localizedFormat);
+dayjs.extend(customParseFormat);
+dayjs.extend(utc);
+dayjs.extend(duration);
+
+dayjs().calendar(null, {
+  sameElse: 'MMM DD, YYYY, h:mm A',
+});
+
+export default dayjs;
+
+export function dateIsValid(date: null | Date): boolean {
+  return dayjs(date).isValid();
+}
